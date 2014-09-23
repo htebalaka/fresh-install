f1c68c99eb97af6f4636f54ffe883b159625a638
Malcolm Gooding <goodingm@gmail.com>
2014-09-23 11:25:11 -0700

    moved ghci config file into its own folder
    deleted makefile. refresh now provides its functionality. also moved some
    function definitions around in refresh, since i think bash depends on the
    ordering of definitions more closely than haskell
    
    added an alias for cabal install, though i don't think it's particularly
    useful. i think what i need is a cabal-nuke, which would use list-sources
    to find what the dependencies of a package are, then do
     * cabal sandbox delete
     * cabal sandbox init
     * cabal sandbox add-source <sources>
     * cabal install --only-dependencies
     * cabal configure
     * cabal build
     * cabal install
     * cabal clean
    not sure about the ordering of `configure` and `install --only-dependencies`.
    either way, i think i'm not entirely convinced the later `install` isn't
    needed; my recent experiences seem to suggest otherwise. but now that i've
    got sandboxes working i should try without the later install to see if the
    issues reappear to better understand what's going on
    
    still experimenting with my keymap in vim. i'm hoping extensive use of
    `noremap` will be the trick to keep things predictable, though i did still
    experience an issue with the <ctrl-w><j/k/l/i> bindings. i changed them
    to arrow keys, which has the same effect, but i'll need to find out what's
    really going on at some point

--- refresh.sh
+++ refresh.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-FRESH_INSTALL="${HOME}/gits/fresh-install"
-
-PROFILE="${FRESH_INSTALL}/profile"
-HOME_PROFILE=$(if_osx "${HOME}/.bash_profile" "${HOME}/.profile")
-PROFILE_ALL="${PROFILE}/profile.all"
-PROFILE_OS="${PROFILE}/profile.$(if_osx "osx" "unix")"
-
-VIM="${FRESH_INSTALL}/vim"
 
 # naive check of which computer im using
 function if_osx {
@@ -16,6 +8,16 @@ function if_osx {
       echo "$2"
    fi
 }
+
+FRESH_INSTALL="${HOME}/gits/fresh-install"
+
+PROFILE="${FRESH_INSTALL}/profile"
+HOME_PROFILE=$(if_osx "${HOME}/.bash_profile" "${HOME}/.profile")
+PROFILE_ALL="${PROFILE}/profile.all"
+PROFILE_OS="${PROFILE}/profile.$(if_osx "osx" "unix")"
+
+VIM="${FRESH_INSTALL}/vim"
+GHCI="${FRESH_INSTALL}/ghci"
 
 # ensures the home profile imports $1
 function import {
@@ -33,4 +35,16 @@ function refresh {
    import "${FRESH_INSTALL}/refresh.sh"
    # update vim
    rsync -rv ${VIM}/ ${HOME}/
+   # update ghci
+   rsync -rv ${GHCI}/ ${HOME}/
+}
+
+function nuke-all {
+   # nuke vim
+   rm -rf ~/.vim
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   rsync -rv ${VIM}/ ${HOME}/
+   vim +PluginInstall +qall
+
+   refresh
 }
--- vim/.vim/plugin/keymap.vim
+++ vim/.vim/plugin/keymap.vim
@@ -1,24 +1,23 @@
 
 " remap jk/kj to escape
-imap jk <Esc>
-imap kj <Esc> 
+inoremap jk <Esc>
+inoremap kj <Esc> 
 
 " remap movement keys
-"map i <Up>
-"map j <Left>
-"map k <Down>
+noremap i <Up>
+noremap j <Left>
+noremap k <Down>
+" remap and `i` and `h` to compensate
+noremap h i
+noremap H I
 
 "remap window movement keys to match up with above
-map <C-h> <C-w>h
-map <C-j> <C-w>j
-map <C-k> <C-w>k
-map <C-l> <C-w>l
-
-" remap and `i` and `h` to compensate
-"noremap h i
-"noremap H I
+noremap <C-j> <C-w><Left>
+noremap <C-k> <C-w><Down>
+noremap <C-l> <C-w><Right>
+noremap <C-i> <C-w><Up>
 
 " retroactive sudo-save
 cmap w!! w !sudo tee % >/dev/null " allow w!! to sudo save
 
-nmap <space> i_<esc>r
+nnoremap <space> i_<esc>r
